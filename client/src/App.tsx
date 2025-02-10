7bcf250931a5c32462695b3e2999bcc9b98905ea
Thurzas <mathieu.miot@gmail.com>
2025-02-10 12:12:39 +0100

    Changed title name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="logo">JS Monorepo</h1>
+        <h1 className="logo">Wild Series</h1>
       </header>
 
       <nav className="navbar">
