00021b163978654a3e4b6061c2fd0e6080b26878
Thurzas <mathieu.miot@gmail.com>
2025-02-10 14:06:56 +0100

    route for programs

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,11 +8,12 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
-
+import programActions from "./modules/program/programActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+router.get("/api/programs",programActions.browse);
 /* ************************************************************************* */
 
 export default router;
